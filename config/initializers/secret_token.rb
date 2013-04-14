6350b9fa70e6765ebeb2a5ddf745151b6defed57
sujimichi <sujimichi@googlemail.com>
2013-04-14 22:09:33 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jebretary::Application.config.secret_token = '8c90cd0f7f11c56fdbdef45e24eb9a810796e82ee464053bee3f0f15bb2267d9828e8310d4a136f7779c9f8f68cf74430e30f24002adbfef8b5985e12df59d75'
