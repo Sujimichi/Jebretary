a874d75c4480c6d24c0d0cbb6faa728624249a76
sujimichi <sujimichi@googlemail.com>
2013-11-17 15:04:38 +0000

    changed entry point for runner

--- build_runner.bat
+++ build_runner.bat
@@ -1,5 +1,4 @@
 r:
 cd r:\
 cd Games
-ocra jebretary\j_liquid.rb jebretary --add-all-core --gemfile jebretary\Gemfile --no-dep-run --gem-full --chdir-first --innosetup jebretary_build\jebretary_installer_script.iss --no-lzma --output jebretary_build\liquid.exe
-
+ocra jebretary\script\rails jebretary --add-all-core --gemfile jebretary\Gemfile --no-dep-run --gem-full --chdir-first --innosetup jebretary_build\jebretary_installer_script.iss --no-lzma --output jebretary_build\liquid.exe -- runner "System.run_monitor" -e production
--- j_liquid.rb
+++
@@ -1,2 +0,0 @@
-puts "Starting Background Monitor(lizard)"
-system "bundle exec rake monitor RAILS_ENV=production" 
