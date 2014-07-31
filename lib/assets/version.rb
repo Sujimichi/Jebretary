d62173d95ba4351381c51eeed97f630724395272
sujimichi <sujimichi@gmail.com>
2014-07-31 21:55:11 +0100

    version bump, new feature

--- lib/assets/version.rb
+++ lib/assets/version.rb
@@ -1,6 +1,6 @@
 module Jebretary
-  VERSION = "0.3.4"
-  RELEASE_VERSION = "0.3.3"
-  RELEASE_URL = "http://www.mediafire.com/download/b2r221bh8bwlxbz/jebretary_setup_v0.3.3.exe"
+  VERSION = "0.4.0"
+  RELEASE_VERSION = "0.3.4"
+  RELEASE_URL = "http://www.mediafire.com/download/b2r221bh8bwlxbz/jebretary_setup_v0.3.4.exe"
 end
 
